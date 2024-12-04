a52b72ce29b2ee47a61366b2b2c6b4aa83ef739f
jakubkaluzik <jkb.kaluzik@gmail.com>
2024-12-04 23:48:00 +0100

    basic-console

---
+++ rusty_shell/src/main.rs
@@ -0,0 +1,10 @@
+mod console;
+use crate::console::{io, run};
+
+fn main() -> io::Result<()> {
+    let mut terminal = ratatui::init();
+    terminal.clear()?;
+    let app_result = run(terminal);
+    ratatui::restore();
+    app_result
+}
